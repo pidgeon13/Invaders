e83a9e3fdb63394b3d3c2e9d0a605012253830e7
Stephen Pidgeon <pidgeon13@googlemail.com>
2020-06-17 20:16:26 +0100

    Added Player that moves and shoots bullets

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Invaders
 Little clone game made using DragonRuby
+
+My first project to get used to using DragonRuby, which you can find here:
+https://dragonruby.itch.io/dragonruby-gtk
---
+++ app/main.rb
@@ -0,0 +1,91 @@
+$gtk.reset
+
+$screen_width = 1280
+$screen_height = 720
+
+class Bullet
+  attr_reader :pos_x
+  attr_reader :pos_y
+  def initialize pos_x, pos_y, dir
+    @pos_x = pos_x
+    @pos_y = pos_y
+    @dir = dir
+    @size = 5
+  end
+  def move
+    @pos_y += (@dir*10)
+  end
+  def render
+    $solids << [@pos_x, @pos_y, @size, @size, 255, 255, 255]
+  end
+end
+
+class Player
+  attr_reader :pos_x
+  attr_reader :pos_y
+  def initialize size
+    @size = size
+    @pos_x =($screen_width - @size) / 2
+    @pos_y = 10
+    @speed = 12
+    @cooldown = 0
+    @max_cooldown = 10
+  end
+  def update
+    if($inputs.keyboard.key_held.left && @pos_x >=@speed)
+      @pos_x-=@speed
+    end
+    if($inputs.keyboard.key_held.right && @pos_x <= $screen_width - @size- @speed)
+      @pos_x+=@speed
+    end
+    if(@cooldown > 0)
+      @cooldown -= 1
+    end
+  end
+  def fire_bullet
+    @cooldown=@max_cooldown
+    return Bullet.new @pos_x + (@size/2), @pos_y +@size, 1
+  end
+  def can_fire
+    return @cooldown <= 0
+  end
+  def render
+    $solids << [@pos_x, @pos_y, @size, @size, 0, 255, 255]
+  end
+end
+
+class InvadersGame
+  def initialize (args)
+    @player = Player.new 30
+    @bullets = []
+  end
+  def render_background
+    $solids << [0,0,$screen_width,$screen_height]
+  end
+  def update_bullets
+    if $inputs.keyboard.key_down.space || $inputs.keyboard.key_held.space
+      if @player.can_fire
+        @bullets << @player.fire_bullet
+      end
+      puts "#{@bullets.size}"
+    end
+    @bullets.delete_if {|bullet| bullet.pos_y >= $screen_height}
+    @bullets.each do |bullet|
+      bullet.move
+      bullet.render
+    end
+  end
+  def tick
+    render_background
+    @player.update
+    @player.render
+    update_bullets
+  end
+end
+
+def tick args
+  $inputs = args.inputs
+  $solids = args.outputs.solids
+  args.state.game ||= InvadersGame.new args
+  args.state.game.tick
+end
