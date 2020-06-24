b4b7a7a4f7423d8c0e65101d6a1404564f9cf057
Stephen Pidgeon <pidgeon13@googlemail.com>
2020-06-24 20:36:07 +0100

    Bullets now destroy enemies and disappear.

--- app/main.rb
+++ app/main.rb
@@ -10,17 +10,22 @@ $game_right_extent=$game_left_extent + $game_width
 class Bullet
   attr_reader :pos_x
   attr_reader :pos_y
+  attr_reader :needs_deleting
   def initialize pos_x, pos_y, dir
     @pos_x = pos_x
     @pos_y = pos_y
     @dir = dir
     @size = 5
+    @needs_deleting = false
   end
   def move
     @pos_y += (@dir*10)
   end
   def render
     $solids << [@pos_x, @pos_y, @size, @size, 255, 0, 255]
+  end
+  def remove
+    @needs_deleting = true
   end
 end
 
@@ -59,36 +64,39 @@ class Player
     return @cooldown <= 0
   end
   def render
-    $sprites << [@pos_x, @pos_y, @size*1.2, @size*1.9, "sprites/kestral.png"]
+    if(!@needs_deleting)
+      $sprites << [@pos_x, @pos_y, @size*1.2, @size*1.9, "sprites/kestral.png"]
+    end
   end
 end
 
 class Enemy_Grid
-  def initialize number_x, number_y, pos_x = $game_left_extent , pos_y = $screen_height, offset = 50
+  def initialize number_x, number_y, left = $game_left_extent, top = $screen_height, size_x = 30
     @number_x = number_x
     @number_y = number_y
-    @pos_x = pos_x
-    @pos_y = pos_y
-    @offset = offset
+    @size_x = size_x
+    @size_y = size_x*(337/259)
+    @pos_x = left
+    @pos_y = top - @number_y * @size_y
     @grid = Array.new(@number_x) {Array.new(@number_y, 1)}
     @velocity = 1
-    @vertical_jump = @offset/2
+    @vertical_jump = @size_y/2
   end
   def render
     for i in 0..@grid.length-1
       column=@grid[i]
-      for j in 1..column.length
+      for j in 0..column.length-1
         if(column[j]!=0)
-          $sprites << [@pos_x+i*@offset, @pos_y - (j+1)*@offset, @offset, @offset, "sprites/Mantis.png"]
+          $sprites << [@pos_x+i*@size_x, @pos_y + j*@size_y, @size_x, @size_y, "sprites/mantis_crop.png"]
         end
       end
     end
   end
   def length
-    return @offset*@number_x
+    return @size_x*@number_x
   end
   def height
-    return @offset*@number_y
+    return @size_x*@number_y
   end
   def needs_to_move_down
     moving_right = @velocity > 0
@@ -108,13 +116,32 @@ class Enemy_Grid
       @pos_x += @velocity
     end
   end
+  def hitting bullet
+    distance_x = bullet.pos_x - @pos_x
+    distance_y = bullet.pos_y - @pos_y
+    if(distance_x < 0 || distance_y < 0)
+      return false
+    end
+    index_x = (distance_x/@size_x).floor
+    index_y = (distance_y/@size_y).floor
+    puts index_x
+    if(index_x >= @number_x || index_y >= @number_y)
+      return false
+    end
+    hitting = false
+    if(@grid[index_x][index_y] == 1)
+      hitting = true
+      @grid[index_x][index_y] = 0
+    end
+    return hitting
+  end
 end
 
 class InvadersGame
   def initialize (args)
     @player = Player.new 30
     @bullets = []
-    @enemies = Enemy_Grid.new 10, 3
+    @enemies = Enemy_Grid.new 15, 4
   end
   def render_background
     $solids << [0,0, $screen_width, $screen_height, 30, 30, 30]
@@ -126,9 +153,12 @@ class InvadersGame
         @bullets << @player.fire_bullet
       end
     end
-    @bullets.delete_if {|bullet| bullet.pos_y >= $screen_height}
+    @bullets.delete_if {|bullet| bullet.pos_y >= $screen_height || bullet.needs_deleting}
     @bullets.each do |bullet|
       bullet.move
+      if(@enemies.hitting bullet)
+        bullet.remove
+      end
       bullet.render
     end
   end
